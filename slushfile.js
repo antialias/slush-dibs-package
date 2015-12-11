969c7d0feb31d54032df2eaed2df5f230e9c049d
Thomas Hallock <thomas@1stdibs.com>
2015-12-11 13:12:21 -0500

    prompting to merge JSON files when possible

--- slushfile.js
+++ slushfile.js
@@ -1,7 +1,6 @@
 "use strict";
 const gulp = require('gulp');
 const install = require('gulp-install');
-const conflict = require('gulp-conflict');
 const template = require('gulp-template');
 const inquirer = require('inquirer');
 const camelCase = require('camelcase');
@@ -24,7 +23,7 @@ gulp.task('default', function (done) {
             answers.camelCasedName = camelCase(answers.name);
             gulp.src(__dirname + '/templates/app/**', {dot:true}) // Note use of __dirname to be relative to generator
                 .pipe(template(answers)) // Lodash template support
-                .pipe(conflict('./')) // Confirms overwrites on file conflicts
+                .pipe(conflictMerge)
                 .pipe(gulp.dest('./')) // Without __dirname here = relative to cwd
                 .pipe(install()) // Run `bower install` and/or `npm install` if necessary
                 .on('end', function () {done();}) // Finished!
